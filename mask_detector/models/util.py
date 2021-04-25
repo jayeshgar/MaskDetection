6142080d21dcd2c876ebae411a11163d81410730
JayeshGarg <jayeshgar@gmail.com>
2021-04-26 05:24:46 +0530

    Bug Fixes

--- mask_detector/models/util.py
+++ mask_detector/models/util.py
@@ -77,7 +77,8 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
 
     x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
     if CUDA:
-        x_y_offset = x_y_offset.detach().cpu().numpy()
+        prediction = prediction.cuda()
+        x_y_offset = x_y_offset.cuda()
 
     prediction[:,:,:2] += x_y_offset
 
